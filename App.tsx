73ad2770546594f38e0160fed4ff3eefc23fbdc7
Alexito <alejandrohortalvalor02@gmail.com>
2024-12-10 17:41:11 +0100

    Cambios en App.tsx para poder acceder a ChangesScreen y asignarle un backgroundcolor y un height

--- App.tsx
+++ App.tsx
@@ -1,13 +1,11 @@
-
-import { NavigationContainer } from '@react-navigation/native';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen';
-import SettingsScreen from './src/screens/SettingsScreen';
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import Ionicons from '@expo/vector-icons/Ionicons';
-
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { StatusBar } from "expo-status-bar";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import HomeScreen from "./src/screens/HomeScreen";
+import ChangesScreen from "./src/screens/ChangesScreen";
+import { ScreenConfigProvider } from "./src/components/ScreenConfigContext"; // Asegúrate de que la ruta sea correcta
 
 const Tab = createBottomTabNavigator();
 
@@ -15,38 +13,54 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator
-      
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          
-          switch(route.name) {
-            case "Home":
-              if (focused) return  ( <Ionicons name="film" size={size} color={color} /> )
-                else return ( <Ionicons name="film-outline" size={size} color={color} /> )
-            case "Settings":
-              if (focused) return ( <Ionicons name="build" size={size} color={color} /> )
-                else return ( <Ionicons name="build-outline" size={size} color={color} /> )
-          }
-
-          return ( <Ionicons name="bulb-outline" size={size} color={color} /> )
-        },
-        tabBarActiveTintColor: '#000000',
-        tabBarInactiveTintColor: '#444444',
-      })}
-      
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            switch (route.name) {
+              case "Home":
+                return (
+                  <Ionicons
+                    name={focused ? "film" : "film-outline"}
+                    size={size}
+                    color={color}
+                  />
+                );
+              case "AnotherHome":
+                return (
+                  <Ionicons
+                    name={focused ? "repeat" : "repeat-outline"}
+                    size={size}
+                    color={color}
+                  />
+                );
+            }
+            return <Ionicons name="bulb-outline" size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "#000000",
+          tabBarInactiveTintColor: "#444444",
+        })}
       >
-        <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'Películas' }}/>
-        <Tab.Screen name="Settings" component={SettingsScreen} options={{ title: 'Configuración' }}/>
+        <Tab.Screen name="Home" options={{ title: "Películas" }}>
+          {() => (
+            <ScreenConfigProvider
+              config={{ height: 300, backgroundColor: "#f5f5f5" }}
+            >
+              <HomeScreen />
+            </ScreenConfigProvider>
+          )}
+        </Tab.Screen>
+        <Tab.Screen
+          name="AnotherHome"
+          options={{ title: "Películas Duplicadas" }}
+        >
+          {() => (
+            <ScreenConfigProvider
+              config={{ height: 200, backgroundColor: "#CD5C5C" }}
+            >
+              <ChangesScreen />
+            </ScreenConfigProvider>
+          )}
+        </Tab.Screen>
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
