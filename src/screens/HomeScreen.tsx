6af97daed1ea253c9d16c1022e85672995123567
Alejandro Hortal <alejandrohortalvalor02@gmail.com>
2024-12-10 13:31:32 +0100

    Implementación del slider infinito en el HomeScreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -6,10 +6,22 @@ import MoviePageButton from "../components/MoviePageButton";
 
 export default function HomeScreen() {
   const { nowPlaying, loading, nextPage } = useMovies();
+  //En el caso de que las películas no estén cargadas, sale un mensaje de "Cargando películas"
+  if (loading)
+    return (
+      <View>
+        <Text style={{ fontSize: 24, fontWeight: "bold", textAlign: "center" }}>
+          Cargando películas
+        </Text>
+      </View>
+    );
+    //He dejado el botón de siguiente página para que haya dos maneras de pasar de página
   return (
     <View>
-      <Text>HomeScreen</Text>
-      <Slider movies={nowPlaying.movies} height={100} />
+      <Text style={{ fontSize: 24, fontWeight: "bold", textAlign: "center" }}>
+        Películas (Página {nowPlaying.page})
+      </Text>
+      <Slider movies={nowPlaying.movies} height={100} onEndReached={nextPage} />
       <MoviePageButton onPress={nextPage} title="Siguiente Página" />
     </View>
   );
